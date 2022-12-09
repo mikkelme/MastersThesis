2008d7a8052ce89851e44c27417590fbb5996eaf
Mikkel Metzsch Jensen <metzschjensen@gmail.com>
2022-12-09 15:13:52 +0100

    At a crossroad. Might be forced to put substrate creation into lammps to get a better framework for multi configuration data generator

---
+++ config_builder/build_substrate.py
@@ -0,0 +1,39 @@
+import sys
+sys.path.append('../') # parent folder: MastersThesis
+
+
+from graphene_sheet.build_utils import *
+
+from ase.lattice.cubic import FaceCenteredCubicFactory
+class DiamondFactory(FaceCenteredCubicFactory):
+    """A factory for creating diamond lattices."""
+    xtal_name = 'diamond'
+    bravais_basis = [[0, 0, 0], [0.25, 0.25, 0.25]]
+
+
+def build_substrate(subpos, a_Si = 5.430953):
+    """ Build substrate object in ASE with 
+        minimum dimensions (Lx, Ly, Lz) = subpos  """
+    
+    # Get lattice size
+    subpos[:2] += a_Si/4 
+    size = np.ceil([pos/a_Si for pos in subpos]).astype('int')
+    
+    # Build
+    Diamond = DiamondFactory()
+    atoms = Diamond(directions = [[1,0,0], [0,1,0], [0,0,1]], 
+                    size = size, 
+                    symbol = 'Si', 
+                    pbc = (1,1,0), 
+                    latticeconstant = a_Si)
+    
+    return atoms
+    
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
