0c0b61cefc33cddd3a5806045e95572373632b7c
Mikkel Metzsch Jensen <metzschjensen@gmail.com>
2023-01-18 10:35:47 +0100

    updated indexing figures

--- produce_figures/indexing.py
+++ produce_figures/indexing.py
@@ -10,16 +10,25 @@ from ase.visualize.plot import plot_atoms
 # https://gitlab.com/ase/ase/blob/master/ase/visualize/plot.py
 
 
-def show_coordinates(shape = (6,6)):
+def common_settings():
+    plt.xlabel(r"$x$ (armchair direction)", fontsize=14)
+    plt.ylabel(r"$y$ (zigzag direction)" , fontsize=14)
+    plt.tight_layout(pad=1.1, w_pad=0.7, h_pad=0.2)
+    
+
+
+def show_coordinates(shape = (6,6), save = False):
     # Build sheet 
     mat = np.ones((shape[0], shape[1])).astype('int')
     builder = config_builder(mat)
     Cdis = 1.461
     a = 3*Cdis/np.sqrt(3)
+    atom_radii = 0.8
+    
     
     # Plot patches
     plt.figure(num=0, dpi=80, facecolor='w', edgecolor='k')
-    ax = plot_atoms(builder.sheet, radii = 0.8, show_unit_cell = 0, scale = 1, offset = (0,0))
+    ax = plot_atoms(builder.sheet, radii = atom_radii, show_unit_cell = 0, scale = 1, offset = (0,0))
 
     # Get min x and y for alligbment 
     xp, yp = [], [] # patches position
@@ -37,17 +46,19 @@ def show_coordinates(shape = (6,6)):
         for j in range(shape[1]):
             x, y = positions[i*shape[1]+j][:2] + (xs, ys)
             verline.append([x,y])
-            plt.text(x, y, f'{i},{j}', horizontalalignment='center', 
+            plt.text(x, y, f'({i},{j})', horizontalalignment='center', 
                                        verticalalignment='center')
 
         verline = np.array(verline)
         plt.plot(verline[:,0], verline[:,1], linewidth = 0.5, linestyle = 'dashed', alpha = 0.75, color = 'grey', zorder = 0)
  
-    
+    Bx = a/(np.sqrt(3)*2)
+    vecax = a*np.sqrt(3)/2
+    vecay = a/2
     
     # Change axis ticks to match coordinate system
-    xtick_loc = np.array([xs + 1/2*a/(2*np.sqrt(3)) + i*a*np.sqrt(3)/2 for i in range(shape[0])])
-    ytick_loc = np.array([ys + j*a*1/2 for j in range(shape[1])])
+    xtick_loc = np.array([xs  + ((i+1)//2)*(vecax+Bx) + ((i)//2)*2*Bx for i in range(shape[0])])
+    ytick_loc = np.array([ys + j*vecay for j in range(shape[1])])
     ax.set_xticks(xtick_loc)
     ax.set_xticklabels(np.arange(len(xtick_loc)))
     ax.set_yticks(ytick_loc)
@@ -57,23 +68,24 @@ def show_coordinates(shape = (6,6)):
     ax.xaxis.grid(False) # vertical lines
     ax.set_axisbelow(True)
     
-    plt.xlabel(r"$x$", fontsize=14)
-    plt.ylabel(r"$y$", fontsize=14)
-    plt.tight_layout(pad=1.1, w_pad=0.7, h_pad=0.2)
-    plt.savefig("../article/figures/atom_indexing.pdf", bbox_inches="tight")
+    common_settings()
+    if save:
+        plt.savefig("../article/figures/atom_indexing.pdf", bbox_inches="tight")
 
 
 
-def show_center_coordinates(shape = (6,6)):
+def show_center_coordinates(shape = (6,6), save = False):
     # Build sheet 
     mat = np.ones((shape[0], shape[1])).astype('int')
     builder = config_builder(mat)
     Cdis = 1.461
     a = 3*Cdis/np.sqrt(3)
+    atom_radii = 0.8
+    center_radii = 0.4
     
     # Plot patches
     plt.figure(num=1, dpi=80, facecolor='w', edgecolor='k')
-    ax = plot_atoms(builder.sheet, radii = 0.8, show_unit_cell = 0, scale = 1, offset = (0,0))
+    ax = plot_atoms(builder.sheet, radii = atom_radii, show_unit_cell = 0, scale = 1, offset = (0,0))
 
     # Get min x and y for alligbment 
     xp, yp = [], [] # patches position
@@ -85,16 +97,24 @@ def show_center_coordinates(shape = (6,6)):
     xs, ys = min(xp), min(yp) # xstart, ystart 
 
 
-    # Plot center elements
-    Lx = ((a*np.sqrt(3)/2) + a/(2*np.sqrt(3)))/2 
+    Bx = a/(np.sqrt(3)*2)
+    vecax = a*np.sqrt(3)/2
+    vecay = a/2
+    
+
+    # Plot center elements and coordinates
+    Lx = (vecax + Bx)/2 
     for j in range(0, shape[1], 2):
         horline = []
         for i in range(0, shape[0]+1):
-            x = xs + (1+3/2*i - 3/2) * Lx
+            x = xs + (1+ 3/2*(i - 1)) * Lx
             y = ys + a*(1/2 + 1/2*j - 1/2*(i%2)) 
             
+            plt.text(x, y, f'({i},{j})', horizontalalignment='center', 
+                                       verticalalignment='center')
+            
             horline.append([x,y])
-            circle = plt.Circle((x, y), 0.2,  color = color_cycle(9))
+            circle = plt.Circle((x, y), center_radii,  color = color_cycle(9))
             ax.add_patch(circle)
 
         horline = np.array(horline)
@@ -103,25 +123,27 @@ def show_center_coordinates(shape = (6,6)):
 
     # Change axis ticks to match center coordinate system
     xtick_loc = [xs + (3*i/2+1) * Lx for i in range(-1, shape[0])]
-    ytick_loc = [ys + 1/2*a*j for j in range(shape[1])]
+    ytick_loc = [ys + vecay*(2*j + 1) for j in range(shape[1]//2)]
     ax.set_xticks(xtick_loc)
     ax.set_xticklabels(np.arange(len(xtick_loc)))
     ax.set_yticks(ytick_loc)
     ax.set_yticklabels(np.arange(len(ytick_loc)))
     
+    
+    ax.set_xlim([xs + -Lx/2 - 3/2*center_radii, xs + (1+ 3/2*(shape[0] - 1)) * Lx + 3/2*center_radii])
+    
     ax.yaxis.grid(False) # horizontal lines
     ax.xaxis.grid(True) # vertical lines
     ax.set_axisbelow(True)
 
-    plt.xlabel(r"$x$", fontsize=14)
-    plt.ylabel(r"$y$", fontsize=14)
-    plt.tight_layout(pad=1.1, w_pad=0.7, h_pad=0.2)
-    
-    plt.savefig("../article/figures/center_indexing.pdf", bbox_inches="tight")
+
+    common_settings()
+    if save:
+        plt.savefig("../article/figures/center_indexing.pdf", bbox_inches="tight")
 
 if __name__ == "__main__":
     shape = (6, 6)
-    show_coordinates(shape)
-    show_center_coordinates(shape)
+    show_coordinates(shape, True)
+    show_center_coordinates(shape, True)
     plt.show()
     
